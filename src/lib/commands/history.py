75220952e7263d6156e3dc51ab6e54aba03731a9
manov <mail>
2015-09-30 22:18:48 +0600

    pyramid and history

--- src/lib/commands/history.py
+++ src/lib/commands/history.py
@@ -1,1 +1,84 @@
+# -*- coding: utf-8 -*-
 __author__ = 'Life'
+
+import requests
+import json
+import time
+
+
+class Channel(object):
+    def __init__(self, name, break_time=1200):
+        self.name = name
+        self.is_online = False
+        self.started = False
+        self.curr_game = ''
+
+        # время offline, за которое список игр не обнуляется (default 20 m)
+        self.break_time = break_time
+
+        # время, когда стрим становится offline
+        self.time_ = 0
+
+        # список игр, которые были на стриме
+        self.games = []
+
+    def get_state(self):
+        req = 'https://api.twitch.tv/kraken/streams/{0}'
+        resp = requests.get(req.format(self.name), timeout=3).json()
+        self.is_online = resp['stream'] is not None
+        self.curr_game = resp['stream']['channel']['game'] if self.is_online else ''
+        #print(self.curr_game)
+
+    def shorten_game(self):
+        short_names = {
+            'Counter-Strike: Global Offensive': 'CS:GO',
+            'Counter-Strike: Source': 'CS Source',
+            'Hearthstone: Heroes of Warcraft': 'Hearthstone',
+            'World of Warcraft': 'WoW'
+        }
+        return short_names.get(self.curr_game, self.curr_game)
+
+    def add_game(self):
+        game_name = self.shorten_game()
+        if self.games:
+            if self.games[-1] != game_name:
+                self.games.append(game_name)
+                #print('Adding >> ' + game_name)
+        else:
+            self.games.append(game_name)
+            #print('Adding >> ' + game_name)
+
+    def expired(self):
+        return time.time() - self.time_ > self.break_time
+
+
+# значение break_time можно и не указывать (20 минут по умолч.)
+ch_list = [
+    Channel('a_o_w', 5*60)
+]
+
+
+def history(args, chan, username):
+    if args and args[0] == 'check':
+        for ch in ch_list:
+            try:
+                ch.get_state()
+            except Exception:  # ConnectionError, ConnectTimeout
+                continue
+            if ch.is_online:
+                ch.add_game()
+                ch.started = True
+            else:
+                if ch.started:
+                    ch.time_ = time.time()
+                    ch.started = False
+                if ch.expired():
+                    ch.games = []
+        return ''
+
+    for ch in ch_list:
+        if ch.name == chan[1:]:
+            if ch.games:
+                return ' -> '.join(ch.games)
+            else:
+                return 'Игор не было'
--- src/lib/commands/pyramid.py
+++ src/lib/commands/pyramid.py
@@ -1,1 +1,56 @@
+# -*- coding: utf-8 -*-
 __author__ = 'Life'
+
+import json
+import random
+
+from src.lib.functions_general import pp
+
+filename = 'src/res/emoticons.txt'
+try:
+    emotes = json.loads(open(filename).read())
+    emotes = [el['regex'] for el in emotes['emoticons'] if el['regex'].isalpha()]
+except Exception:
+    pp('Ошибка при обработке файла смайлов (будут исп. несколько известных)', 'error')
+    emotes = ['Kappa', 'Keepo', 'OpieOP', 'duDudu']
+
+turned_on = {
+    '#c_a_k_e': False,
+    '#nastjanastja': False,
+    '#adrenaline_life': False,
+    '#a_o_w': False
+}
+
+allowed_users = (
+    'a_o_w',
+    'adrenaline_life',
+    'c_a_k_e',
+    'nastjanastja'
+)
+
+
+def pyramid(args, chan, username):
+    if args[0] in ('on', 'off'):
+        if username in allowed_users:
+            turned_on[chan] = True if args[0] == 'on' else False
+        return ''
+
+    if turned_on[chan] or username in allowed_users:
+        try:
+            length = int(args[0])
+        except Exception:
+            return ''
+
+        if length > 5:
+            length = 5
+        if length < 2:
+            length = 2
+
+        if len(args) == 2:
+            word = args[1]
+        else:
+            word = random.choice(emotes)
+        word += ' '
+        res = [word * i for i in range(1, length + 1)]
+        res += res[-2::-1]
+        return res
