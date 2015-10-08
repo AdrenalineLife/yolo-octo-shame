a0c3bf1fd87261eb6259267b1bbe43e3590a9025
manov <mail>
2015-10-09 00:17:25 +0600

    fixes

--- src/lib/commands/ragnaros.py
+++ src/lib/commands/ragnaros.py
@@ -6,17 +6,18 @@ import random
 import json
 
 
-color = 'Red'  # color for using me
-hit_freq = 5*60  # ban every hit_freq seconds
+color = 'Red'  # color for using /me
+hit_freq = float(90)  # ban every hit_freq seconds
 last_time_hit = 0
-get_in_trap_time = 30
-ban_time = 2*60
+get_in_trap_time = float(30)
+ban_time = 40
 turned_on = False
 victims = dict()
 
 allowed = ('c_a_k_e', 'a_o_w', 'nastjanastja')
-say = ('/me Рагнарос выходит на стол!', '/me Рагнарос покидает доску!')
+protected = ('c_a_k_e', 'a_o_w', 'nastjanastja', 'seeskixbocta', 'moobot', 'mirrobot', 'etozhemad')
 
+say = ('/me Рагнарос выходит на стол!', '/me Рагнарос покидает доску!')
 
 # ban someone
 def make_hit():
@@ -37,33 +38,49 @@ switch_state = lambda x: True if x == 'on' else False
 def ragnaros(args, chan, username):
     global turned_on
     global last_time_hit
+    global hit_freq
+    global ban_time
     if chan == '#c_a_k_e':
-        if args and args[0] == 'check':
-            #print('>>> checking')
-            remove = [x for x in victims if time.time() - victims[x] >= get_in_trap_time]
-            for x in remove:
-                victims.pop(x, None)
+        if args:
+            if args[0] == 'check':
+                #print('>>> checking')
+                remove = [x for x in victims if time.time() - victims[x] >= get_in_trap_time]
+                for x in remove:
+                    victims.pop(x, None)
 
-            #print(json.dumps(victims, indent=4))
+                #print(json.dumps(victims, indent=4))
 
-            if turned_on and time.time() - last_time_hit >= hit_freq:
-                #print('>> It is time to ban someone!')
-                last_time_hit = time.time()
-                return make_hit()
-            return ''
+                if turned_on and time.time() - last_time_hit >= hit_freq:
+                    #print('>> It is time to ban someone!')
+                    last_time_hit = time.time()
+                    return make_hit()
+                return ''
 
-        if args and args[0] == 'add':
-            if args[1] not in allowed:
-                victims[args[1]] = time.time()
-            return ''
+            if args[0] == 'add':
+                if args[1] not in protected:
+                    victims[args[1]] = time.time()
+                return ''
 
-        if args and args[0] in ('on', 'off') and username in allowed:
-            turned_on = switch_state(args[0])
-            return [
-                '/color {0}'.format(color),
-                say[0] if turned_on else say[1],
-                '/color Blue'
-            ]
+            if args[0] in ('on', 'off') and username in allowed:
+                turned_on = switch_state(args[0])
+                return [
+                    '/color {0}'.format(color),
+                    say[0] if turned_on else say[1],
+                    '/color Blue'
+                ]
+
+            if len(args) == 2 and username in allowed:
+                if args[0] in ('cd', 'bantime'):
+                    try:
+                        sec = int(args[1])
+                    except ValueError:
+                        return ''
+                    if sec > 4:
+                        if args[0] == 'cd':
+                            hit_freq = float(sec)
+                        else:
+                            ban_time = sec
+                return ''
 
         if username in allowed:
             return make_hit()
