c4411992318078effe94513dbf394af7c25bfe2b
manov <mail>
2016-02-15 00:48:22 +0600

    fix

--- src/res/Message_class.py
+++ src/res/Message_class.py
@@ -2,21 +2,27 @@
 __author__ = 'Life'
 
 import re
+import traceback
 
 
 class Message(object):
     def __init__(self, data, expl=False, name=None, mes=None, chan_=None):
         if not expl:
-            self.name = re.findall(r'@([a-zA-Z0-9_]+)[.]tmi[.]twitch[.]tv PRIVMSG', data)[0]
-            self.disp_name = re.findall(r';display-name=([a-zA-Z0-9_]*)\\?[a-z]?;', data)[0]
-            if not self.disp_name:
-                self.disp_name = self.name
-            self.message = re.findall(r'@[a-zA-Z0-9_]+[.]tmi[.]twitch[.]tv PRIVMSG #[a-z0-9_]+ :(.*)', data)[0]
-            self.chan = re.findall(r'@[a-zA-Z0-9_]+[.]tmi[.]twitch[.]tv PRIVMSG (.*?) :', data)[0]
-            self.color = re.findall(r'^@color=([A-Z0-9#]*);', data)[0]
-            self.is_mod = (re.findall(r';mod=([01]);', data)[0] == '1') or (self.chan[1:] == self.name)
-            self.is_sub = re.findall(r';subscriber=([01]);', data)[0] == '1'
-            self.is_turbo = re.findall(r';turbo=([01]);', data)[0] == '1'
+            try:
+                self.name = re.findall(r'@([a-zA-Z0-9_]+)[.]tmi[.]twitch[.]tv PRIVMSG', data)[0]
+                self.disp_name = re.findall(r';display-name=([a-zA-Z0-9_]*)\\?[a-z]?;', data)[0]
+                if not self.disp_name:
+                    self.disp_name = self.name
+                self.message = re.findall(r'@[a-zA-Z0-9_]+[.]tmi[.]twitch[.]tv PRIVMSG #[a-z0-9_]+ :(.*)', data)[0]
+                self.chan = re.findall(r'@[a-zA-Z0-9_]+[.]tmi[.]twitch[.]tv PRIVMSG (.*?) :', data)[0]
+                self.color = re.findall(r'^@color=([A-Z0-9#]*);', data)[0]
+                self.is_mod = (re.findall(r';mod=([01]);', data)[0] == '1') or (self.chan[1:] == self.name)
+                self.is_sub = re.findall(r';subscriber=([01]);', data)[0] == '1'
+                self.is_turbo = re.findall(r';turbo=([01]);', data)[0] == '1'
+            except Exception as ee:
+                err_msg = data + '\n' + traceback.format_exc()
+                print('>>>>>>>>>>>>>\n', err_msg)
+                self.name, self.disp_name, self.chan, self.message = '', '', '#', ''
         else:
             self.disp_name = name
             self.name = name
