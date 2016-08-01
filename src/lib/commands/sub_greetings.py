3d3bf7f74b0623c2629a9243530fcc2ebe087a25
manov <mail>
2016-08-02 00:50:52 +0700

    emotes for sub greetings

--- src/lib/commands/sub_greetings.py
+++ src/lib/commands/sub_greetings.py
@@ -5,11 +5,13 @@ __author__ = 'Life'
 greetings = {
     '#c_a_k_e': {
         'new': 'Приветствуем новобранца {n}!',
-        'resub': 'Спасибо за подписку, {n}! {m} месяцев'
+        'resub': 'Спасибо за подписку, {n}! {m} месяцев {e}',
+        'emote': 'Kappa'
     },
     '#nastjanastja': {
-        'new': 'Спасибо, {n}!',
-        'resub': 'Спасибо за подписку, {n}! {m} месяцев'
+        'new': 'Thank you, {n}!',
+        'resub': 'Thanks {n} for {m} month! {e}',
+        'emote': 'Keepo'
     },
     '#': {
         'new': '',
@@ -22,6 +24,12 @@ def sub_greetings(self, args, msg):
     chan, name, month = args
     try:
         resp = greetings[chan]['resub'] if month else greetings[chan]['new']
+        emote = greetings[chan]['emote'].strip() + ' '
+        str_kwargs = {
+            'm': month,
+            'n': name,
+            'e': (emote * month).rstrip()
+        }
     except KeyError:
         return ''
-    return resp.format(n=name, m=month) if month else resp.format(n=name)
+    return resp.format(**str_kwargs)
