15e41e01ce94d010c871d93dc485260c5f76f61b
AdrenalineLife <ristonl1fe@gmail.com>
2017-04-03 23:03:42 +0700

    improvements on plugsong.py

--- src/lib/commands/plugsong.py
+++ src/lib/commands/plugsong.py
@@ -12,7 +12,7 @@ from src.lib.functions_general import pp
 from src.res.authorization import *
 
 
-class LoginError(Exception):
+class PlugLoginError(Exception):
     pass
 
 
@@ -43,12 +43,11 @@ def get(path, return_req=False, **kwargs):
 
 def login(email, password):
     get_root = session.get(urljoin(rest_url_base, "/"))
-    resp = get_root.text
-    csrf = js_var("_csrf", resp)
+    csrf = js_var("_csrf", get_root.text)
     if csrf is None:
-        raise LoginError
-    json = {"csrf": csrf, "email": email, "password": password}
-    return post("auth/login", json=json)
+        raise PlugLoginError
+    json_ = {"csrf": csrf, "email": email, "password": password}
+    return post("auth/login", json=json_)
 
 
 def join_room(room):
@@ -78,54 +77,85 @@ not_logged = True
 
 
 plug_rooms = {
-    '#nastjanastja': '',
-    '#1': 'nightcore-331',
+    '#1': '',
     '#2': 'i-the-80-s-and-90-s-1'
 }
 
 
 def plugsong(self, args, msg):
-    global not_logged
+    global not_logged, plug_rooms
+
     try:
         if not_logged:
-            if login(mail, passw)['status'] != 'ok':
+            if login(mail, passw)['status'] != 'ok':  # logging and checking
                 pp('Failed logging to PlugDJ', 'ERROR')
                 return 'Ошибка подключения к PlugDJ'
             else:
                 not_logged = False
-        if msg.chan not in plug_rooms:
+        if msg.chan not in plug_rooms:  # if plugdj room for this twitch chan is not provided
             return ''
         else:
-            join_room(plug_rooms[msg.chan])
+            join_room(plug_rooms[msg.chan])  # we should join a room before getting its state
         state = room_state()
-    except LoginError:
+
+    except PlugLoginError:
         not_logged = True
         pp('Failed logging to PlugDJ: csrf is None', 'ERROR')
         return 'Ошибка подключения к PlugDJ'
     except Exception as detail:
         not_logged = True
-        pp('Failed logging to PlugDJ: %s' % detail, 'ERROR')
+        pp('Failed logging to PlugDJ: {}'.format(detail), 'ERROR')
         return 'Ошибка подключения к PlugDJ'
 
-    #print(json.dumps(state, sort_keys=True, indent=4, separators=(',', ': ')))
+    #print(json.dumps(state, sort_keys=True, indent=4))
 
     if state['status'] != 'ok':
         not_logged = True
         pp('Failed getting room state', 'ERROR')
         return 'Ошибка подключения к PlugDJ'
+    else:
+        state = state['data'][0]
 
-    if state['data'][0]['playback']:
-        seconds = state['data'][0]['playback']['media']['duration']
+    playback = state['playback']  # info about current song
+    if playback:  # determining the form of the response
+        if args and args[0] == 'room':
+            resp = '{author} — {title} [{dur}]. DJ: {dj}. Людей: {ppl}. Очередь: {queue} ({url})'
+        else:
+            resp = 'Сейчас играет на {url}: {author} — {title} [{dur}]. DJ: {dj}'
+    else:
+        if args and args[0] == 'room':
+            resp = 'Людей: {ppl}. Очередь: {queue} ({url})'
+        else:
+            return 'В данный момент музыка на PlugDJ не играет'
+
+    if playback:  # if music is playing
+        seconds = playback['media']['duration']
         m, s = divmod(seconds, 60)
         h, m = divmod(m, 60)
-        timestr = '%02d:%02d:%02d' % (h, m, s) if h else '%02d:%02d' % (m, s)
-        resp = 'Сейчас играет на PlugDJ: %s - %s [%s]' % (state['data'][0]['playback']['media']['author'],
-                                                          state['data'][0]['playback']['media']['title'],
-                                                          timestr)
-        djname = name_of_user(state['data'][0]['booth']['currentDJ'])
-        if djname:
-            resp += '. DJ: %s' % djname
-        return resp
-    else:
-        return 'В данный момент музыка на PlugDJ не играет'
+        time_str = '{0:0>2}:{1:0>2}:{2:0>2}'.format(h, m, s) if h else '{0:0>2}:{1:0>2}'.format(m, s)
 
+        dj_id = state['booth']['currentDJ']
+        dj_name = next((x['username'] for x in state['users'] if x['id'] == dj_id), None)
+        dj_name = name_of_user(dj_id) if dj_name is None else dj_name
+
+        return resp.format(author=playback['media']['author'],
+                           title=playback['media']['title'],
+                           dur=time_str,
+                           dj=dj_name,
+                           ppl=state['meta']['population'],
+                           queue=len(state['booth']['waitingDJs']),
+                           url='https://plug.dj/' + plug_rooms[msg.chan])
+
+    else:
+        return resp.format(ppl=state['meta']['population'],
+                           queue=len(state['booth']['waitingDJs']),
+                           url='plug.dj/' + plug_rooms[msg.chan])
+
+
+if __name__ == '__main__':  # little bit of testing
+    class MSG(object):  # fake message class
+        pass
+    msg_ = MSG()
+    msg_.chan = '#2'
+    print(plugsong(None, [], msg_))
+    print(plugsong(None, ['room'], msg_))
