4b39a961c65c231d4a9de2efbca86eaed4fd7986
AdrenalineLife <ristonl1fe@gmail.com>
2017-08-10 23:45:26 +0700

    fixes for bgrank

--- src/lib/commands/bgrank.py
+++ src/lib/commands/bgrank.py
@@ -5,8 +5,6 @@ import requests
 import json
 import time
 
-#from bs4 import BeautifulSoup
-
 from src.lib.functions_general import pp
 
 bg_nick = {  # PUBG nickname
@@ -14,7 +12,8 @@ bg_nick = {  # PUBG nickname
     '#': ''
 }
 
-default_type = {  # keys are lowcase
+# default region for the channel
+default_reg = {  # keys are lowcase
     '#c_a_k_e': 'eu',
     '#': ''
 }
@@ -30,17 +29,17 @@ type_full = {  # value is lowercase
 
 USAGE = '/w (sender) !bgrank <type> [<region>]'
 LINK = 'https://pubgtracker.com/api/profile/pc/{}'
-API_KEY = '97d234e8-3d38-41ab-ba53-b5ef39ac7a95'
-SEASON = '2017-pre2'
+API_KEY = ''
 RESP = 'PUBG {type} {region}: Rank {Rank}, Rating {Rating}, Rounds {RoundsPlayed}, Wins {Wins}, Kills {Kills}, Top 10: {Top10s}'
 
 
 def bgrank(self, args, msg):
     nick = bg_nick.get(msg.chan, None)
     if nick is None:
+        pp('No PUBG nickname found for channel {}'.format(msg.chan), mtype='WARNING')
         return ''
 
-    reg = default_type.get(msg.chan, 'na')  # region
+    reg = default_reg.get(msg.chan, 'na')  # region
     if args:
         if args[0].lower() in ('s', 'd', 'sq', 'solo', 'duo', 'squad'):
             type_ = args[0].lower()
@@ -56,22 +55,35 @@ def bgrank(self, args, msg):
     try:
         response = requests.get(LINK.format(nick),
                                 headers={'TRN-Api-Key': API_KEY},
-                                timeout=2.6).json()
+                                timeout=2.4).json()
+
         if 'error' in response:
-            print(response.get('message', 'No message in response'))
+            pp('Error in response: {} (bgrank.py)'.format(
+                response.get('message', response.get('error', 'No message'))), mtype='WARNING')
             return None
 
-        stats = next((x['Stats'] for x in response['Stats']
-                      if x['Season'] == SEASON and x['Region'] == reg and x['Match'] == type_))
-        stats_ = {x['field']: x['displayValue'] for x in stats}
+        season = response['defaultSeason']  # rename it carefull, used later through locals().get()
+        stats = next((x['Stats'] for x in response['Stats']  # all stats for curr season, region and type
+                      if x['Season'] == season and x['Region'] == reg and x['Match'] == type_))
+        stats_ = {x['field']: x['value'] for x in stats}
         stats_.update(Rank=next(str(x['rank']) for x in stats if x['field'] == 'Rating'))
 
-    except requests.RequestException:
-        pp('Request exception (bgrank.py)', mtype='WARNING')
+    except requests.Timeout:
+        pp('Request timeout (bgrank.py)', mtype='WARNING')
         return None
-    except (KeyError, ValueError) as e:
-        pp('{}: {} key is missing (bgrank.py)'.format(e.__class__.__name__, str(e)), mtype='WARNING')
+    except requests.RequestException as e:
+        pp('{}: {} (bgrank.py)'.format(e.__class__.__name__, str(e)[:70]), mtype='WARNING')
         return None
+    except KeyError as e:
+        pp('KeyError: {} key is missing (bgrank.py)'.format(str(e)), mtype='WARNING')
+        return None
+    except ValueError as e:
+        pp('ValueError: {} (bgrank.py)'.format(str(e)), mtype='WARNING')
+        return None
+    except StopIteration:
+        return 'Не найдено игр на {type} {region} {season}'.format(type=type_.capitalize(),
+                                                                   region=reg.upper(),
+                                                                   season=locals().get('season', ''))  # tricky
 
     return RESP.format(type=type_.capitalize(), region=reg.upper(), **stats_)
 
@@ -86,6 +98,7 @@ if __name__ == '__main__':
     class m: pass
     n = m(); n.chan = '#c_a_k_e'
     print(bgrank(None, ['solo'], n)); time.sleep(2)
+    print(bgrank(None, ['duo'], n)); time.sleep(2)
     print(bgrank(None, ['DUO', 'na'], n)); time.sleep(2)
-    print(bgrank(None, ['SQuad', 'eu'], n))
+    #print(bgrank(None, ['SQuad', 'eu'], n))
 
